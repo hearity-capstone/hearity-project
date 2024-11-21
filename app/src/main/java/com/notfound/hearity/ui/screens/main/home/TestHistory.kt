cc330759b8a6e6c23a60b28a508e057452324077
Eko Kurniawan <57644404+ekokurnia@users.noreply.github.com>
2024-11-21 13:59:10 +0700

    feat: test history section
    - create test history card

--- app/src/main/java/com/notfound/hearity/ui/screens/main/home/TestHistory.kt
+++ app/src/main/java/com/notfound/hearity/ui/screens/main/home/TestHistory.kt
@@ -2,16 +2,16 @@ package com.notfound.hearity.ui.screens.main.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import com.notfound.hearity.ui.theme.PaddingSmall
+import com.notfound.hearity.ui.theme.PaddingMedium
+import com.notfound.hearity.ui.theme.SpacingItem
 import com.notfound.hearity.ui.theme.SpacingSmall
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -40,9 +40,7 @@ fun TestHistory(modifier: Modifier = Modifier) {
 
         testResults.forEach { result ->
             TestHistoryItem(result)
-            Spacer(modifier = Modifier.height(SpacingSmall))
-            HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
-            Spacer(modifier = Modifier.height(SpacingSmall))
+            Spacer(modifier = Modifier.height(SpacingItem))
         }
     }
 }
@@ -51,33 +49,38 @@ fun TestHistory(modifier: Modifier = Modifier) {
 fun TestHistoryItem(result: TestResult) {
     val formattedDate = result.testDate.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"))
 
-    Column(
+    Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { }
-            .padding(PaddingSmall)
+            .clip(MaterialTheme.shapes.large)
+            .clickable { },
     ) {
-        Text(
-            text = formattedDate,
-            style = MaterialTheme.typography.labelMedium,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-        )
-        Spacer(Modifier.height(SpacingSmall))
-        Text(
-            text = result.doctorName,
-            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
-            color = MaterialTheme.colorScheme.onSurface,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
-        )
-        Spacer(Modifier.height(SpacingSmall))
-        Text(
-            text = result.summary,
-            style = MaterialTheme.typography.bodySmall,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis
-        )
+        Column(
+            modifier = Modifier.padding(PaddingMedium)
+        ) {
+            Text(
+                text = formattedDate,
+                style = MaterialTheme.typography.labelMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+            )
+            Spacer(Modifier.height(SpacingSmall))
+            Text(
+                text = result.doctorName,
+                style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+                color = MaterialTheme.colorScheme.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(Modifier.height(SpacingSmall))
+            Text(
+                text = result.summary,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+
     }
 }
 
