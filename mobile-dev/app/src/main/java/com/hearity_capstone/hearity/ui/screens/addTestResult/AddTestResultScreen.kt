d1a0cbc4c549e9addf41ea7367ed1e57de8ccbc3
Eko Kurniawan <57644404+ekokurnia@users.noreply.github.com>
2024-12-08 19:46:38 +0700

    Feat: Add test result screen UI updates
    - Added a save button to the top bar of the Add Test Result screen.
    - Changed the "Place of practice" field to "Hospital or clinic name".
    - Added a section for selecting a test result image.

--- mobile-dev/app/src/main/java/com/hearity_capstone/hearity/ui/screens/addTestResult/AddTestResultScreen.kt
+++ mobile-dev/app/src/main/java/com/hearity_capstone/hearity/ui/screens/addTestResult/AddTestResultScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Apartment
+import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material.icons.outlined.Event
+import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.CalendarLocale
 import androidx.compose.material3.DatePicker
@@ -20,6 +22,7 @@ import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -32,6 +35,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
+import com.hearity_capstone.hearity.ui.common.AppButton
 import com.hearity_capstone.hearity.ui.common.AppTextField
 import com.hearity_capstone.hearity.ui.common.AppTopBar
 import com.hearity_capstone.hearity.ui.theme.PaddingMedium
@@ -44,7 +48,7 @@ import java.time.LocalDate
 @Composable
 fun AddTestResultScreen(navController: NavHostController, modifier: Modifier = Modifier) {
     val doctorName = remember { mutableStateOf("") }
-    val placeOfPractice = remember { mutableStateOf("") }
+    val hospitalName = remember { mutableStateOf("") }
     val selectedDate = remember { mutableStateOf<LocalDate>(LocalDate.now()) }
 
     val openDialog = remember { mutableStateOf(false) }
@@ -83,7 +87,17 @@ fun AddTestResultScreen(navController: NavHostController, modifier: Modifier = M
 
     Scaffold(
         modifier = modifier,
-        topBar = { AppTopBar(navController = navController, title = "Add Test Result") }
+        topBar = {
+            AppTopBar(navController = navController, title = "Add Test Result", action = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Outlined.Check,
+                        contentDescription = null,
+                    )
+
+                }
+            })
+        }
     ) {
         Box(
             modifier = Modifier
@@ -113,9 +127,9 @@ fun AddTestResultScreen(navController: NavHostController, modifier: Modifier = M
                     Spacer(Modifier.width(SpacingSection))
                     AppTextField(
                         modifier = Modifier.fillMaxWidth(),
-                        value = placeOfPractice.value,
-                        label = "Place of practice",
-                        onValueChange = { v -> placeOfPractice.value = v })
+                        value = hospitalName.value,
+                        label = "Hospital or clinic name",
+                        onValueChange = { v -> hospitalName.value = v })
                 }
                 Spacer(Modifier.height(SpacingSection))
                 Row(verticalAlignment = Alignment.CenterVertically) {
@@ -141,7 +155,16 @@ fun AddTestResultScreen(navController: NavHostController, modifier: Modifier = M
                         )
                     )
                 }
-                Spacer(Modifier.height(SpacingItem))
+                Spacer(Modifier.height(SpacingSection))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Outlined.Image,
+                        contentDescription = null,
+                    )
+                    Spacer(Modifier.width(SpacingSection))
+                    AppButton(onClick = {}, label = "Select Test Result Image")
+                }
             }
         }
     }
