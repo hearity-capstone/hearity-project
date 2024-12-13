14c01b3d34986a82e996a796b2b995d9b4c9a32c
Nopalogic <naufaladhi02@gmail.com>
2024-12-13 14:21:30 +0700

    fix(logic): correct conditional logic and remove unnecessary line

--- cloud-computing/src/services/forecast.service.js
+++ cloud-computing/src/services/forecast.service.js
@@ -1,17 +1,16 @@
 import bigQuery from '../config/database.config.js';
 import { createResponse } from '../helpers/createResponse.js';
 
-
 const getForecastResult = async (id) => {
 	const options = {
 		query: `SELECT * FROM \`hearity.forecasting\` WHERE user_id = @value`,
 		params: { value: id },
 	};
 	const [forecastResult] = await bigQuery.query(options);
-	
-	if (forecastResult === undefined) return createResponse(404, 'Forecast result not found');
 
-	return createResponse(200, 'Success get forecast result', { data: forecastResult });
+	if (forecastResult.length === 0) return createResponse(404, 'Forecast result not found');
+
+	return createResponse(200, 'Success get all forecast result', { data: forecastResult });
 };
 
 export default { getForecastResult };
