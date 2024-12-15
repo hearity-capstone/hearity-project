5c29eb9eaf65c08b9179afac1dd33653e8d8b9ef
Eko Kurniawan <57644404+ekokurnia@users.noreply.github.com>
2024-12-15 23:13:07 +0700

    Feat: Enhance Sign Up Screen with Additional Fields and Validation
    - Added new fields to the Sign Up screen, including First Name, Last Name, Phone Number, Address, City, and Gender.
    - Implemented input validation for the new fields using regex patterns.
    - Updated the UI to display error messages when the input is invalid.
    - Refactored the Sign Up form to use the new validation logic.
    - Added a Gender radio button group for selecting gender.
    - Added icons to the input fields for better user experience.
    - Updated spacing and layout for improved visual appeal.

--- mobile-dev/app/src/main/java/com/hearity_capstone/hearity/ui/screens/authentication/SignUpScreen.kt
+++ mobile-dev/app/src/main/java/com/hearity_capstone/hearity/ui/screens/authentication/SignUpScreen.kt
@@ -2,6 +2,7 @@ package com.hearity_capstone.hearity.ui.screens.authentication
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -9,11 +10,18 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationCity
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,7 +32,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.hearity_capstone.hearity.R
@@ -45,7 +55,7 @@ import com.hearity_capstone.hearity.util.ValidatorUtils
 
 @Composable
 fun SignUpScreen(
-    navController: NavController
+    navController: NavController,
 ) {
 
 
@@ -101,23 +111,37 @@ private fun SignUpForm(
     navController: NavController,
     modifier: Modifier = Modifier,
 ) {
-    var name by remember { mutableStateOf("") }
+    val genderOptions = listOf("Male", "Female")
+
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
+    var phoneNumber by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("Male") }
+    var address by remember { mutableStateOf("") }
+    var city by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var repeatPassword by remember { mutableStateOf("") }
 
     var isPasswordVisible by remember { mutableStateOf(false) }
     var isRepeatPasswordVisible by remember { mutableStateOf(false) }
-
-    var isNameValid by remember { mutableStateOf(true) }
+    var isFirstNameValid by remember { mutableStateOf(true) }
+    var isLastNameValid by remember { mutableStateOf(true) }
     var isEmailValid by remember { mutableStateOf(true) }
+    var isPhoneNumberValid by remember { mutableStateOf(true) }
+    var isAddressValid by remember { mutableStateOf(true) }
+    var isCityValid by remember { mutableStateOf(true) }
     var isPasswordValid by remember { mutableStateOf(true) }
     var isRepeatPasswordValid by remember { mutableStateOf(true) }
 
 
     fun validateForm() {
-        isNameValid = ValidatorUtils.validateName(name)
+        isFirstNameValid = ValidatorUtils.validateName(firstName)
+        isLastNameValid = ValidatorUtils.validateName(lastName)
         isEmailValid = ValidatorUtils.validateEmail(email)
+        isPhoneNumberValid = ValidatorUtils.validatePhoneNumber(phoneNumber)
+        isAddressValid = ValidatorUtils.validateAddress(address)
+        isCityValid = ValidatorUtils.validateCity(city)
         isPasswordValid = ValidatorUtils.validatePassword(password)
         isRepeatPasswordValid = repeatPassword == password
     }
@@ -126,14 +150,27 @@ private fun SignUpForm(
         modifier = modifier,
     ) {
         AppTextField(
-            value = name,
-            label = "Name",
+            value = firstName,
+            label = "First Name",
             onValueChange = { value ->
-                name = value
-                isNameValid = ValidatorUtils.validateName(value)
+                firstName = value
+                isFirstNameValid = ValidatorUtils.validateName(value)
             },
             modifier = Modifier.fillMaxWidth(),
-            isError = !isNameValid,
+            isError = !isFirstNameValid,
+            leadingIcon = {
+                Icon(Icons.Filled.Person, contentDescription = "Person Icon")
+            }
+        )
+        AppTextField(
+            value = lastName,
+            label = "Last Name",
+            onValueChange = { value ->
+                lastName = value
+                isFirstNameValid = ValidatorUtils.validateName(value)
+            },
+            modifier = Modifier.fillMaxWidth(),
+            isError = !isLastNameValid,
             leadingIcon = {
                 Icon(Icons.Filled.Person, contentDescription = "Person Icon")
             }
@@ -147,7 +184,8 @@ private fun SignUpForm(
             isError = !isEmailValid,
             modifier = Modifier.fillMaxWidth(),
         )
-        Spacer(Modifier.height(SpacingItem))
+
+        Spacer(Modifier.height(SpacingSection))
         AppPasswordTextField(
             value = password,
             onValueChange = { value ->
@@ -173,19 +211,111 @@ private fun SignUpForm(
             isError = !isRepeatPasswordValid,
             onPasswordVisibilityChange = { isRepeatPasswordVisible = !isRepeatPasswordVisible }
         )
+        Spacer(Modifier.height(SpacingSection))
+        AppTextField(
+            value = phoneNumber,
+            label = "Phone Number",
+            onValueChange = { value ->
+                phoneNumber = value
+                isPhoneNumberValid = ValidatorUtils.validatePhoneNumber(value)
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Phone),
+            modifier = Modifier.fillMaxWidth(),
+            isError = !isPhoneNumberValid,
+            leadingIcon = {
+                Icon(Icons.Filled.Phone, contentDescription = "Phone Icon")
+            }
+        )
+        AppTextField(
+            value = address,
+            label = "Address",
+            onValueChange = { value ->
+                address = value
+                isAddressValid = ValidatorUtils.validateAddress(value)
+            },
+            modifier = Modifier.fillMaxWidth(),
+            isError = !isAddressValid,
+            leadingIcon = {
+                Icon(Icons.Filled.Home, contentDescription = "Address Icon")
+            })
+        AppTextField(
+            value = city,
+            label = "City",
+            onValueChange = { value ->
+                city = value
+                isCityValid = ValidatorUtils.validateAddress(value)
+            },
+            modifier = Modifier.fillMaxWidth(),
+            isError = !isCityValid,
+            leadingIcon = {
+                Icon(Icons.Filled.LocationCity, contentDescription = "City Icon")
+            })
+        Spacer(Modifier.height(SpacingItem))
+        GenderRadio(
+            genderOptions = genderOptions,
+            selectedOption = gender,
+            onOptionSelected = { gender = it }
+        )
         Spacer(Modifier.height(SpacingSectionLarge))
 
         AppButton(
             onClick = {
                 validateForm()
-                if (isNameValid && isEmailValid && isPasswordValid && isRepeatPasswordValid) {
+                if (isFirstNameValid && isEmailValid && isPasswordValid && isRepeatPasswordValid) {
                     navController.navigateToLogin()
                 }
             },
-            enabled = isNameValid && isEmailValid && isPasswordValid && isRepeatPasswordValid,
+            enabled = isFirstNameValid && isEmailValid && isPasswordValid && isRepeatPasswordValid,
             modifier = Modifier.fillMaxWidth(),
             label = "Sign Up",
         )
+        Spacer(Modifier.height(SpacingItem))
+        AppButton(
+            onClick = {
+                navController.popBackStack()
+            },
+            modifier = Modifier.fillMaxWidth(),
+            label = "Back To Login",
+        )
 
+    }
+}
+
+
+@Composable
+private fun GenderRadio(
+    genderOptions: List<String>,
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit,
+) {
+
+    Column(Modifier.selectableGroup()) {
+        Text("Gender", style = MaterialTheme.typography.titleSmall)
+        genderOptions.forEach { text ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .selectable(
+                        selected = (text == selectedOption),
+                        onClick = {
+                            onOptionSelected(text)
+                        },
+                        role = Role.RadioButton
+                    )
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    onClick = null
+                )
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.bodyMedium.merge(),
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+        }
     }
 }
