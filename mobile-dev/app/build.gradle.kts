2202c95551964e55e06d9d25598ccc54e191c8e9
Eko Kurniawan <57644404+ekokurnia@users.noreply.github.com>
2024-12-17 11:43:22 +0700

    Chore: Bump app version to 0.7.0-beta
    - Updated the app version code to 0.7.0-beta in the build.gradle.kts file.

--- mobile-dev/app/build.gradle.kts
+++ mobile-dev/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1"
+        versionName = "0.7.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
